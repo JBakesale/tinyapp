8869374b1c6d154313af65abc9f16300add4e208
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-16 01:28:13 -0700

    functions debugged and exported

--- express_server.js
+++ express_server.js
@@ -1,65 +1,26 @@
+// Module Imports:
+
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 3333;
 
-// middleware
+// Function Imports:
+const {
+  getUserByEmail,
+  generateRandomString,
+  urlsForUser,
+} = require("./helpers");
+
+// Middleware
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// configuration
+// Configuration
 app.set("view engine", "ejs");
-
-// functions => move all over to helpers.js at the end and require('/helpers.js)
-function generateRandomString(length) {
-  const uniqueId = Math.random()
-    .toString(36)
-    .substring(2, length + 2);
-  return uniqueId;
-}
-
-const getUserByEmail = function (email, database) {
-  for (const userId in database) {
-    const user = database[userId];
-    if (user.email === email) {
-      return user;
-    }
-  }
-
-  return null;
-}; // this function hasn't been used... do i need it? >> check later and replace findUser eventually
-
-const findUser = (email) => {
-  for (const userId in users) {
-    const user = database[userId];
-    if (user.email === email) {
-      return user;
-    }
-  }
-
-  return null;
-};
-
-const urlsForUser = (shortId) => {
-  const urls = {};
-  const ids = Object.keys(urlDatabase);
-
-  console.log("ids:", ids);
-
-  for (const id of ids) {
-    const url = urlDatabase[id];
-    if (id === shortId) {
-      urls[id] = url;
-    }
-  }
-  //almost works but doesn't display more than one url!!
-  //hasnt been fully tested since database update
-
-  return urls;
-};
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
@@ -78,13 +39,13 @@ const urlDatabase = {
 };
 
 const users = {
-  aJ48lW: {
-    id: "aJ48lW",
+  abc: {
+    id: "abc",
     email: "alice@a.com",
     password: bcrypt.hashSync("lilbandit", 10),
   },
-  def: {
-    id: "def",
+  aJ48lW: {
+    id: "aJ48lW",
     email: "bob@b.com",
     password: "$2b$12$DCMzn1YLRocNWAB0acDzYOf827/cqZLUb8.mB1.oQJzhC0H7rCUTS",
   },
@@ -154,13 +115,12 @@ app.get("/urls/:id", (req, res) => {
     // this will also likely need to be updated post urlDatabase update
   };
 
-// POST /urls/:id should return a relevant error message if id does not exist
-// POST /urls/:id should return a relevant error message if the user is not logged in
-// POST /urls/:id should return a relevant error message if the user does not own the URL
-// POST /urls/:id/delete should return a relevant error message if id does not exist
-// POST /urls/:id/delete should return a relevant error message if the user is not logged in
-// POST /urls/:id/delete should return a relevant error message if the user does not own the URL.
-
+  // POST /urls/:id should return a relevant error message if id does not exist
+  // POST /urls/:id should return a relevant error message if the user is not logged in
+  // POST /urls/:id should return a relevant error message if the user does not own the URL
+  // POST /urls/:id/delete should return a relevant error message if id does not exist
+  // POST /urls/:id/delete should return a relevant error message if the user is not logged in
+  // POST /urls/:id/delete should return a relevant error message if the user does not own the URL.
 
   res.render("urls_show", templateVars);
 });
@@ -239,7 +199,7 @@ app.post("/login", (req, res) => {
       .send("Something went wrong. <a href='/login'>Please try again.</a>");
   }
 
-  const foundUser = findUser(email);
+  const foundUser = getUserByEmail(email, urlDatabase);
 
   // keep status vague for security
   if (!foundUser) {
@@ -278,7 +238,7 @@ app.post("/register", (req, res) => {
       .send("No input detected. <a href='/register'>Please try again</a>");
   }
 
-  const foundUser = findUser(email);
+  const foundUser = getUserByEmail(email, urlDatabase);
 
   // keep response vague for security
   if (foundUser) {
--- helpers.js
+++ helpers.js
@@ -1,4 +1,40 @@
+const getUserByEmail = function (email, database) {
+  for (const urlId in database) {
+    const urlEntry = database[urlId];
+    const userId = urlEntry.userID;
+    const user = users[userId];
 
-//require these exports in my express_server.js file
-//move helper functions in here and export
-module.exports( {functions} );
+    if (user.email === email) {
+      return user.id;
+    }
+    return null;
+  }
+};
+function generateRandomString(length) {
+  const uniqueId = Math.random()
+    .toString(36)
+    .substring(2, length + 2);
+  return uniqueId;
+}
+const urlsForUser = (userId) => {
+  const urls = {};
+  for (const urlId in urlDatabase) {
+    const urlEntry = urlDatabase[urlId];
+    if (urlEntry.userID === userId) {
+      urls[urlId] = urlEntry.longURL;
+    }
+  }
+  return urls;
+};
+
+module.exports = {
+  getUserByEmail,
+  generateRandomString,
+  urlsForUser,
+};
+
+// const {
+//   getUserByEmail,
+//   generateRandomString,
+//   urlsForUser,
+// } = require("./helpers");
