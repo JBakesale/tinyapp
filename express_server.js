54bff5ee6cf69d6cd4435ee8c1eea8a5c339e2cd
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-19 14:50:55 -0700

    cleaned code for improved readability and removal of unnecessary code

--- express_server.js
+++ express_server.js
@@ -24,7 +24,8 @@ app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
 
-// Databases
+// Databases:
+  //urlDatabase with prefilled test urls and Ids
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
@@ -35,6 +36,7 @@ const urlDatabase = {
     userID: "aJ48lW",
   },
 };
+  //users database with prefilled test users
 const users = {
   abc: {
     id: "abc",
@@ -63,25 +65,7 @@ const urlsForUser = (userId) => {
   return urls;
 };
 
-// GET
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/urls/:id/show", (req, res) => {
-  const TinyURL = urlDatabase.req.params;
-  const longURL = urlDatabase[TinyURL].longURL;
-  const userId = req.session.userId;
-  const user = users[userId];
-
-  const templateVars = {
-    TinyURL,
-    longURL,
-    user,
-  };
-  return res.render("urls_show", templateVars);
-});
-
+// Create New Url Page
 app.get("/urls/new", (req, res) => {
   const userId = req.session.userId;
   const user = users[userId];
@@ -101,6 +85,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+// URL Database for signed in User
 app.get("/urls", (req, res) => {
   const userId = req.session.userId;
   const user = users[userId];
@@ -119,6 +104,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// URL Edit Page with signed in User check
 app.get("/urls/:id", (req, res) => {
   const userId = req.session.userId;
   const user = users[userId];
@@ -143,6 +129,7 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// Login Page
 app.get("/login", (req, res) => {
   const userId = req.session.userId;
   const user = users[userId];
@@ -158,6 +145,7 @@ app.get("/login", (req, res) => {
   return res.render("urls_login", templateVars);
 });
 
+// Registration Page
 app.get("/register", (req, res) => {
   const userId = req.session.userId;
   const user = users[userId];
@@ -166,6 +154,7 @@ app.get("/register", (req, res) => {
   return res.render("urls_register", templateVars);
 });
 
+// Login Redirect for root Url entry
 app.get("/", (req, res) => {
   const userId = req.session.userId;
   const user = users[userId];
@@ -181,7 +170,7 @@ app.get("/", (req, res) => {
   return res.render("urls_login", templateVars);
 });
 
-// POST
+// Redirect for New Url creation
 app.post("/urls", (req, res) => {
   const { longURL } = req.body;
   const id = generateRandomString(6);
@@ -193,17 +182,20 @@ app.post("/urls", (req, res) => {
       "You must be logged in to view this page. <a href='/login'>Login here.</a>"
     );
   }
+  // Add new Url to the secure database
   urlDatabase[id] = { longURL, userID: userId };
 
   return res.redirect("/urls");
 });
 
+// URL deleted from database with redirect to URL database page
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect("/urls");
 });
 
+// Edit URL by User => redirect to URL page with updated Database
 app.post("/urls/:id", (req, res) => {
   const { newUrl } = req.body;
   const shortId = req.params.id;
@@ -217,6 +209,7 @@ app.post("/urls/:id", (req, res) => {
   return res.redirect("/urls");
 });
 
+// Login Validation
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -246,14 +239,16 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+// Logout and nullify session credentials 
 app.post("/logout", (req, res) => {
-  // Clear cookies
-  req.session.userId = undefined;
+  req.session.userId = null;
   return res.redirect("/login");
 });
 
+// New Account Registration
 app.post("/register", (req, res) => {
   const email = req.body.email;
+  console.log(email);
   const password = bcrypt.hashSync(req.body.password, 10);
 
   if (!email || !password) {
@@ -262,15 +257,16 @@ app.post("/register", (req, res) => {
       .send("No input detected. <a href='/register'>Please try again</a>");
   }
 
-  const foundUser = getUserByEmail(email, urlDatabase);
+  const foundUser = getUserByEmail(email, users);
 
+  // If User already exists
   if (foundUser) {
     return res
       .status(400)
       .send("Something went wrong. <a href='/register'>Please try again</a>");
   }
 
-  // Create new url edit
+  // Create new url id
   const id = generateRandomString(6);
   const newUser = {
     id: id,
